76ca2f834b7f153324bfc7d1cc7214768a200d0a
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2023-12-02 23:24:14 -0500

    logging url

--- src/Kanbas/users/signin.js
+++ src/Kanbas/users/signin.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
 function Signin() {
-    const [ credentials, setCredentials ] = useState({username: '', password: ''});
+    const [credentials, setCredentials] = useState({ username: '', password: '' });
     const navigate = useNavigate();
     const signin = async () => {
         const user = await client.signin(credentials);
@@ -12,13 +12,15 @@ function Signin() {
         console.log("User signed in");
         console.log(user);
         navigate('/Kanbas/Account');
+        const redirectedUrl = window.location.href;
+        console.log("Redirected to:", redirectedUrl);
         console.log("Redirected to Account");
     };
     return (
-        <div style={{display: "flex", flexDirection: "column", width:"400px"}}>
+        <div style={{ display: "flex", flexDirection: "column", width: "400px" }}>
             <h1>Signin</h1>
-            <input type="text" value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})} />
-            <input type="password" value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})} />
+            <input type="text" value={credentials.username} onChange={(e) => setCredentials({ ...credentials, username: e.target.value })} />
+            <input type="password" value={credentials.password} onChange={(e) => setCredentials({ ...credentials, password: e.target.value })} />
             <button onClick={signin}>Signin</button>
             <Link to="../signup">Sign Up</Link>
         </div>
